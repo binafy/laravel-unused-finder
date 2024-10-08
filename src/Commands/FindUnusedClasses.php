e6560d869e43704969ae829996ca09c0a0e53751
Milwad <milwad.dev@gmail.com>
2024-10-08 23:49:39 +0330

    wip

--- src/Commands/FindUnusedClasses.php
+++ src/Commands/FindUnusedClasses.php
@@ -3,6 +3,9 @@
 namespace Binafy\LaravelUnusedFinder\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use function Laravel\Prompts\text;
 
 class FindUnusedClasses extends Command
 {
@@ -21,10 +24,33 @@ class FindUnusedClasses extends Command
     protected $description = 'Find unused classes';
 
     /**
+     * The directory path.
+     *
+     * @var string
+     */
+    protected string $path;
+
+    /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): void
     {
-        
+        if (! $this->option('path')) {
+            $this->path = text('Write the directory path:', default: 'app', required: true);
+        }
+
+        collect($this->path)->each(function ($path) {
+            $phpFiles = collect(File::allFiles($path))
+                ->filter(fn ($filename) => Str::endsWith($filename, '.php'))
+                ->each(function ($phpFile) {
+                    $fileContents = file_get_contents($phpFile);
+                    dd($fileContents);
+                    if (preg_match('/class\s+(\w+)/', $fileContents, $className) === 1) {
+                        $this->classNames[$className[1]] = $phpFile->getPathName();
+                        $fileContents = str_replace($className[1], Str::random(16), $fileContents);
+                    }
+                    $this->massiveString .= $fileContents;
+                });
+            });
     }
 }
