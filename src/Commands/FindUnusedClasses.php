654b5ebf430f9a2ca2dfb0bb67af731502a19a14
Milwad <milwad.dev@gmail.com>
2024-10-08 23:26:25 +0330

    Create FindUnusedClasses.php

---
+++ src/Commands/FindUnusedClasses.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Binafy\LaravelUnusedFinder\Commands;
+
+use Illuminate\Console\Command;
+
+class FindUnusedClasses extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'find-unused:classes';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Find unused classes';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+    }
+}
