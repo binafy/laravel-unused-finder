cf28b82e15f6ffd337814054ee7a9fafbb115d42
Milwad <milwad.dev@gmail.com>
2024-10-08 22:25:31 +0330

    Create LaravelUnusedFinderServiceProvider.php

---
+++ src/Providers/LaravelUnusedFinderServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Binafy\LaravelUnusedFinder\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelUnusedFinderServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        
+    }
+}
