1e0c4719fa87f51081a71bc6e33217339bcae2b0
Milwad <milwad.dev@gmail.com>
2024-10-13 22:24:04 +0330

    Create laravel-unused-finder.php

---
+++ config/laravel-unused-finder.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+     * Unused Classes.
+     */
+    'classes' => [
+        /*
+         * Except some files to check it is used or not.
+         */
+        'except' => [
+            'app/Http/Commands/FindUnusedClasses.php',
+        ],
+    ]
+];
